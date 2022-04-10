80ab6be809a64ebeaef5443be75e0c3d1f1fc75a
jyao244 <jyao244@aucklanduni.ac.nz>
2022-04-10 17:06:05 +1200

    start project

---
+++ lib/home_page.dart
@@ -0,0 +1,297 @@
+import 'dart:convert';
+
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/video.dart';
+// import 'package:get/get.dart';
+import 'colors.dart' as color;
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List subjects=[];
+
+  _initData() async {
+    await DefaultAssetBundle.of(context).loadString("json/subjects.json").then((value){
+      setState(() {
+        subjects=json.decode(value);
+      });
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _initData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "khan Academy",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: color.AppColor.homePageTitle,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                Expanded(child: Container()),
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons),
+                SizedBox(width:10),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  size: 20,
+                  color: color.AppColor.homePageIcons),
+                SizedBox(width:15),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 20,
+                  color: color.AppColor.homePageIcons)
+              ]
+            ),
+            SizedBox(height:30),
+            Row(
+              children: [
+                Text(
+                  "Your program",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageSubtitle,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                Expanded(child: Container()),
+                Text(
+                  "Details",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: color.AppColor.homePageDetail,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(width: 5),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Video()),
+                    );
+                  },
+                  child: Icon(
+                    Icons.arrow_forward,
+                    size: 20,
+                    color: color.AppColor.homePageIcons
+                  ),
+                )
+              ]
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    color.AppColor.gradientFirst,
+                    color.AppColor.gradientSecond
+                  ],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.centerRight
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(80)
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(5,10),
+                    blurRadius: 20,
+                    color: color.AppColor.gradientSecond.withOpacity(0.2)
+                  )
+                ]
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left:20, top:25, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Begin your study",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: color.AppColor.homePageContainerTextBig
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    Text(
+                      "Time is money, no time to lose",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: color.AppColor.homePageContainerTextBig
+                      ),
+                    ),
+                    SizedBox(height: 45),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Icon(Icons.timer, size:20, color: color.AppColor.homePageContainerTextSmall),
+                        SizedBox(width: 10),
+                        Text(
+                          "60 min",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: color.AppColor.homePageContainerTextBig
+                          ),
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(60),
+                            boxShadow: [
+                              BoxShadow(
+                                color: color.AppColor.gradientFirst,
+                                blurRadius: 10,
+                                offset: Offset(4,8)
+                              )
+                            ]
+                          ),
+                          child: Icon(
+                            Icons.play_circle_fill,
+                            color: Colors.white,
+                            size: 60
+                          ),
+                        )
+                      ]
+                    )
+                  ]
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Text(
+                  "Subjects",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w500,
+                    color: color.AppColor.homePageTitle
+                  )
+                )
+              ],
+            ),
+            Expanded(child: OverflowBox(
+              maxWidth: MediaQuery.of(context).size.width,
+              child: ListView.builder(
+                itemCount: (subjects.length.toDouble()/2).toInt(),
+                itemBuilder: (_, i){
+                  int a = 2*i;
+                  int b = 2*i+1;
+                  return Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(left: 30,bottom: 30),
+                        padding: EdgeInsets.only(bottom: 5),
+                        height: 200,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage(
+                              subjects[a]['img']
+                            )
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 3,
+                              offset: Offset(5,5),
+                              color: color.AppColor.gradientSecond.withOpacity(0.1)
+                            ),
+                            BoxShadow(
+                              blurRadius: 3,
+                              offset: Offset(-5,-5),
+                              color: color.AppColor.gradientSecond.withOpacity(0.1)
+                            )
+                          ]
+                        ),
+                        child: Center(
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              subjects[a]['title'],
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: color.AppColor.homePageDetail
+                              )
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(left: 30,bottom: 30),
+                        padding: EdgeInsets.only(bottom: 5),
+                        height: 200,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage(
+                              subjects[b]['img']
+                            )
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              blurRadius: 3,
+                              offset: Offset(5,5),
+                              color: color.AppColor.gradientSecond.withOpacity(0.1)
+                            ),
+                            BoxShadow(
+                              blurRadius: 3,
+                              offset: Offset(-5,-5),
+                              color: color.AppColor.gradientSecond.withOpacity(0.1)
+                            )
+                          ]
+                        ),
+                        child: Center(
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Text(
+                              subjects[b]['title'],
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: color.AppColor.homePageDetail
+                              )
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                }),
+            )
+            )
+        ],)
+      )
+    );
+  }
+}
---
+++ lib/video.dart
@@ -0,0 +1,314 @@
+import 'dart:convert';
+
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
+// import 'package:myapp/home_page.dart';
+import 'colors.dart' as color;
+
+class Video extends StatefulWidget {
+  const Video({ Key key }) : super(key: key);
+
+  @override
+  State<Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<Video> {
+  // VideoPlayerController _controller;
+  bool _play=false;
+  List videos=[];
+
+  _initData() async {
+    await DefaultAssetBundle.of(context).loadString("json/videos.json").then((value){
+      setState(() {
+        videos=json.decode(value);
+      });
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _initData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.AppColor.gradientFirst,
+              color.AppColor.gradientSecond
+            ],
+            begin: const FractionalOffset(0.0, 0.4),
+            end: Alignment.topRight
+          )
+        ),
+        child: Column(
+          children: [
+            _play==false? Container(
+              padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size:20,
+                          color: color.AppColor.secondPageIconColor
+                        ),
+                      ),
+                      Expanded(child: Container()),
+                      Icon(
+                        Icons.info_outline,
+                        size:20,
+                        color: color.AppColor.secondPageIconColor
+                      )
+                    ]
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    "Begin your study",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: color.AppColor.homePageContainerTextBig
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Row(
+                    children: [
+                      Container(
+                        width: 90,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [
+                              color.AppColor.secondPageContainerGradient1stColor,
+                              color.AppColor.secondPageContainerGradient2ndColor
+                            ],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight
+                          )
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "60 min",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: color.AppColor.secondPageIconColor
+                              )
+                            )
+                          ]
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Container(
+                        width: 200,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                            colors: [
+                              color.AppColor.secondPageContainerGradient1stColor,
+                              color.AppColor.secondPageContainerGradient2ndColor
+                            ],
+                            begin: Alignment.bottomLeft,
+                            end: Alignment.topRight
+                          )
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.handyman_outlined,
+                              size: 20,
+                              color: color.AppColor.secondPageIconColor
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "See your study plan",
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: color.AppColor.secondPageIconColor
+                              )
+                            )
+                          ]
+                        ),
+                      )
+                    ]
+                  )
+                ],
+              ),
+            ):Container(
+              child: Column(
+                children: [
+                  Container(
+                    height: 100,
+                    padding: const EdgeInsets.only(top: 30, left:30, right:30),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: (){
+                            debugPrint("tapped");
+                          },
+                          child: Icon(
+                            Icons.arrow_back_ios,
+                            size: 20,
+                            color: color.AppColor.secondPageIconColor)
+                        ),
+                        Expanded(child: Container()),
+                        Icon(
+                          Icons.info_outline,
+                          size: 20,
+                          color: color.AppColor.secondPageIconColor)
+                      ]
+                    )
+                  ),
+                  // _playVideo(context),
+                ]
+              ),
+            ),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(70)
+                )
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      SizedBox(width: 20,),
+                      Text(
+                        "Lesson 1 : Amino acids and proteins",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: color.AppColor.circuitsColor
+                        )
+                      )
+                    ]
+                  ),
+                  SizedBox(height: 20),
+                  Expanded(child: ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    itemCount: videos.length,
+                    itemBuilder: (_, int i){
+                    return GestureDetector(
+                      onTap: (){
+                        // _onTapVideo(i);
+                        debugPrint(i.toString());
+                        setState(() {
+                          if(_play==false){
+                            _play=true;
+                          }
+                        });
+                      },
+                      child: Container(
+                        height: 135,
+                        width: 200,
+                        color: Colors.white,
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  width: 80,
+                                  height: 80,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        videos[i]["picture"]
+                                      ),
+                                      fit: BoxFit.cover
+                                    )
+                                  ),
+                                ),
+                                SizedBox(width: 10),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      videos[i]["title"],
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold
+                                      )
+                                    ),
+                                    SizedBox(height: 10,),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 3),
+                                      child: Text(
+                                        videos[i]["time"],
+                                        style: TextStyle(
+                                          color: Colors.grey[500]
+                                        )
+                                      )
+                                    )
+                                  ]
+                                )
+                              ]
+                            )
+                          ]
+                        ),
+                      ),
+                    );
+                  }))
+                ]
+              ),
+            ))
+          ]
+        ),
+      ),
+    );
+  }
+
+  // _playVideo(BuildContext context){
+  //   final controller = _controller;
+  //   if(controller!=null&&controller.value.initialized){
+  //     return Container(
+  //       height: 300,
+  //       width: 300,
+  //       child: VideoPlayer(controller)
+  //     );
+  //   }else{
+  //     return Text("Please wait");
+  //   }
+  // }
+
+  // _onTapVideo(int i){
+  //   final controller = VideoPlayerController.asset(videos[i]["video"]);
+  //   _controller=controller;
+  //   setState(() {
+      
+  //   });
+  //   controller..initialize().then((_){
+  //     controller.play();
+  //       setState(() {
+          
+  //       });
+  //   });
+  // }
+}
